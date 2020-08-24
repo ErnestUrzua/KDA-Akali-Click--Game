163b92c16e9885985d05cf03612e75acb33614dd
LoneStar <ernieurzua@gmail.com>
2020-08-24 09:55:08 -0700

    Updates

---
+++ precache-manifest.cf91c404a99cc244ff1a5e7936b1ffc4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be9c458485dd88d37605a0865f551c92",
+    "url": "/kda-akali-click-game/index.html"
+  },
+  {
+    "revision": "4090856f5a6cf86793a6",
+    "url": "/kda-akali-click-game/static/css/main.72bcd119.chunk.css"
+  },
+  {
+    "revision": "28b8ddb573501e1b2c8d",
+    "url": "/kda-akali-click-game/static/js/2.88df24ad.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/kda-akali-click-game/static/js/2.88df24ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4090856f5a6cf86793a6",
+    "url": "/kda-akali-click-game/static/js/main.0080517e.chunk.js"
+  },
+  {
+    "revision": "5b70a6dcae1607dcc44a",
+    "url": "/kda-akali-click-game/static/js/runtime-main.c0bd61f1.js"
+  }
+]);
